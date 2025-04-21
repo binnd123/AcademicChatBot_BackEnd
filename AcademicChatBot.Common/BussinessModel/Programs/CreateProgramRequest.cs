c9742a4ab0f7ae48072932ffd2d9bb6f210521af
Nguyễn Đình Bin <binnd26052002@gmail.com>
2025-04-21 09:08:39 +0700

    New Branch

---
+++ AcademicChatBot.Common/BussinessModel/Programs/CreateProgramRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicChatBot.Common.BussinessModel.Programs
+{
+    public class CreateProgramRequest
+    {
+        public string ProgramCode { get; set; } = null!;
+        public string ProgramName { get; set; } = null!;
+        public DateTime StartAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ AcademicChatBot.Service/Contract/IProgramService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcademicChatBot.Common.BussinessModel;
+using AcademicChatBot.Common.BussinessModel.Subjects;
+
+namespace AcademicChatBot.Service.Contract
+{
+    public interface IProgramService
+    {
+        Task<Response> GetAllPrograms(int pageNumber, int pageSize, string search);
+        Task<Response> GetProgramById(Guid subjectId);
+        public Task<Response> CreateProgram(CreateSubjectRequest request);
+        public Task<Response> UpdateProgram(Guid SubjectId, UpdateSubjectRequest request);
+        public Task<Response> DeleteProgram(Guid SubjectId);
+    }
+}
